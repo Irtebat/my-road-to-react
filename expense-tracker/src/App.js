61dcd05f5fc391560eca16e155039155de671a77
Irtebat <irtebat.10@gmail.com>
2021-08-18 02:19:13 +0530

    Added Expenses component : comprises of ExpenseItem, ExpenseDate component. Wrapped up Expenses component in Card Component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,9 +1,17 @@
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  const expenses = [
+    { id: 1, title: "House Rent", amount: 14999, date: new Date(2021, 7, 2) },
+    { id: 2, title: "Car Insurance", amount: 5000, date: new Date(2021, 7, 4) },
+    { id: 3, title: "Utilities", amount: 7899, date: new Date(2021, 6, 28) },
+  ];
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <div className="container-fluid">
+      <Expenses items={expenses}></Expenses>
     </div>
   );
-}
+};
 
 export default App;
---
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -0,0 +1,22 @@
+import Card from "../Wrapper/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      {props.items.map((item) => {
+        return (
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+            key={item.id}
+          ></ExpenseItem>
+        );
+      })}
+    </Card>
+  );
+};
+
+export default Expenses;
